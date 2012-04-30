edad9268ee26595cdbb026c4d72637c8b8e6c063
Dan Hensgen <dan@methodhead.com>
2012-04-30 09:21:17 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demicard::Application.config.secret_token = 'a34c64051705b4be236f1b876048d0967e262396ce6466bf315251328910a01ffb28cc8f5e2abbcab78e0adeae67e6ab718383ab16068f7bf1e94e95a4bac41f'
