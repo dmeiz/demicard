025df4514dc0fae7c4d643ae7589802f672d3d26
Dan Hensgen <dan@methodhead.com>
2012-05-01 16:07:08 -0400

    Chef rubygems

--- config/cookbooks/demicard/recipes/default.rb
+++ config/cookbooks/demicard/recipes/default.rb
@@ -7,12 +7,43 @@ include_recipe 'iptables'
 iptables_rule 'iptables_ssh'
 
 package 'ruby1.9.1'
-package 'rubygems1.9.1'
 package 'apache2'
 
 link '/usr/bin/ruby' do
   to '/usr/bin/ruby1.9.1'
 end
+
+remote_file '/home/vagrant/rubygems-1.8.24.tgz' do
+  source 'http://rubyforge.org/frs/download.php/76073/rubygems-1.8.24.tgz'
+  mode '0644'
+  action :create_if_missing
+end
+
+execute 'tar xzf rubygems-1.8.24.tgz' do
+  cwd '/home/vagrant'
+  command 'tar xzf rubygems-1.8.24.tgz'
+  creates '/home/vagrant/rubygems-1.8.24'
+end
+
+execute 'ruby setup.rb' do
+  cwd '/home/vagrant/rubygems-1.8.24'
+  command 'ruby setup.rb'
+  creates '/usr/bin/gem1.9.1'
+end
+
+link '/usr/bin/gem' do
+  to '/usr/bin/gem1.9.1'
+end
+
+#execute 'gem install passenger' do
+#  command 'gem install passenger'
+#  action :run
+#end
+#
+#execute 'passenger-install-apache2-module' do
+#  command 'passenger-install-apache2-module'
+#  action :run
+#end
 #include_recipe 'openssl'
 #include_recipe 'java' 
 #include_recipe 'postgresql::server' 
