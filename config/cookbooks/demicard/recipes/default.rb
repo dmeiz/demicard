6d847ca0f942ddd32b4b553f2053e81b41f290fc
Dan Hensgen <dan@methodhead.com>
2012-05-01 15:37:01 -0400

    Chef ruby

--- config/cookbooks/demicard/recipes/default.rb
+++ config/cookbooks/demicard/recipes/default.rb
@@ -5,6 +5,14 @@
 #
 include_recipe 'iptables'
 iptables_rule 'iptables_ssh'
+
+package 'ruby1.9.1'
+package 'rubygems1.9.1'
+package 'apache2'
+
+link '/usr/bin/ruby' do
+  to '/usr/bin/ruby1.9.1'
+end
 #include_recipe 'openssl'
 #include_recipe 'java' 
 #include_recipe 'postgresql::server' 
